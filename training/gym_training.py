2e01d3f06bd7a87ccad299160ab2899481bb5ad3
OleguerCanal <oleguer.canal@hotmail.com>
2020-04-07 21:08:50 +0200

    More tests on model saving

--- training/gym_training.py
+++ training/gym_training.py
@@ -1,5 +1,7 @@
 import gym
 import tensorflow as tf
+from tensorflow.python.framework import graph_util
+from tensorflow.python.platform import gfile
 # from gym_unity.envs import UnityEnv
 from gym_unity.envs import UnityEnv
 # from mlagents.envs import UnityEnvironment
@@ -9,26 +11,75 @@ from stable_baselines.ppo1 import PPO1
 from stable_baselines.ppo2 import PPO2
 import mlagents.trainers.tensorflow_to_barracuda as tf2bc
 import numpy as np
-from model_saver import *
+# from model_saver import *
 import os
 import shutil
+from typing import Any, List, Set, NamedTuple
 
 # behavior_name="CarBehavior"
 
-def generate_checkpoint_from_model(model, checkpoint_name):
-    with model.graph.as_default():
-        if os.path.exists(checkpoint_name):
-            shutil.rmtree(checkpoint_name)
+#NOTE: I think we have to change these two frozen sets to match oru graph names
+POSSIBLE_OUTPUT_NODES = frozenset(
+    ["action", "action_probs", "recurrent_out", "value_estimate"]
+)
 
-        tf.saved_model.simple_save(model.sess, checkpoint_name,
-                                    inputs={"obs": model.act_model.obs_ph},
-                                    outputs={"action": model.action_ph})
+MODEL_CONSTANTS = frozenset(
+    ["action_output_shape", "is_continuous_control", "memory_size", "version_number"]
+)
+
+def _process_graph(graph: tf.Graph) -> List[str]:
+    """
+    Gets the list of the output nodes present in the graph for inference
+    :return: list of node names
+    """
+    all_nodes = [x.name for x in graph.as_graph_def().node]
+    print("############")
+    print(all_nodes)
+    nodes = [x for x in all_nodes if x in POSSIBLE_OUTPUT_NODES | MODEL_CONSTANTS]
+    print("List of nodes to export for brain TODO(oleguer put name here)")
+    print("############")
+    print(nodes)
+    print("############")
+    for n in nodes:
+        print("\t" + n)
+    return nodes
+
+def save(model, path):
+    graph = model.graph
+    print(graph)
+    sess = model.sess
+
+    # Make frozen graph
+    with graph.as_default():
+        target_nodes = ",".join(_process_graph(graph))
+        graph_def = graph.as_graph_def()
+        output_graph_def = graph_util.convert_variables_to_constants(
+            sess, graph_def, target_nodes.replace(" ", "").split(","))
+    frozen_graph_def = output_graph_def  # Useless step but I dont wanna change all the names
+
+    # Save frozen graph
+    frozen_graph_def_path = path + "/frozen_graph_def.pb"
+    with gfile.GFile(frozen_graph_def_path, "wb") as f:
+        f.write(frozen_graph_def.SerializeToString())
+
+    # Convert to barracuda
+    tf2bc.convert(frozen_graph_def_path, path + ".nn")
+        
+
+# def generate_checkpoint_from_model(model, checkpoint_name):
+#     with model.graph.as_default():
+#         if os.path.exists(checkpoint_name):
+#             shutil.rmtree(checkpoint_name)
+
+#         tf.saved_model.simple_save(model.sess, checkpoint_name,
+#                                     inputs={"obs": model.act_model.obs_ph},
+#                                     outputs={"action": model.action_ph})
 
 if __name__ == "__main__":
     print("ok")
     env = UnityEnv(
         environment_filename="../Builds/Test.x86_64",
-        worker_id=5026,
+        worker_id=5028,
         no_graphics=False,
         multiagent=False)
     print("ok")
@@ -83,12 +134,11 @@ if __name__ == "__main__":
                 )
     print("saving")
 
-    attrs = vars(model)
-    # {'kids': 0, 'name': 'Dog', 'color': 'Spotted', 'age': 10, 'legs': 2, 'smell': 'Alot'}
-    # now dump this in some way or another
-    print(', '.join("%s: %s" % item for item in attrs.items()))
+    # attrs = vars(model)
+    # print(', '.join("%s: %s" % item for item in attrs.items()))
 
     # model.save("ppo_reacher")
-    save_to_pb(model, 'temp.pb')
+    # save_to_pb(model, 'temp.pb')
     # generate_checkpoint_from_model(model, "temp")
-    tf2bc.convert("temp.pb", "learned_behavior.nn")
+    # tf2bc.convert("temp.pb", "learned_behavior.nn")
+    save(model, "barracuda_test")
